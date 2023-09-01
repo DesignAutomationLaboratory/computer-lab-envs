06c6bc0bc7fa894cad395166bd4a069df0609462
Olle Vidner <olle@vidner.se>
2023-09-01 17:20:58 +0200

    Less formatting cruft

--- tmkt79/tmkt79-environment.bat
+++ tmkt79/tmkt79-environment.bat
@@ -7,23 +7,24 @@ REM be tracked and made during the courses without needing to instruct
 REM students to download a new version of this file.
 
 setlocal
-set PROMPT_PREFIX=[91m~[93m~[92m~[0m
+set PROMPT_PREFIX=[101;93m
+set PROMPT_SUFFIX=[0m
 set ARCHIVE_URL=https://github.com/ovidner/computer-lab-envs/archive/refs/heads/main.zip
 set ARCHIVE_PATH=%TEMP%\computer-lab-envs.zip
 REM When extracting, a subfolder will be created
 set EXTRACT_PATH=%LOCALAPPDATA%
 set INIT_SCRIPT_PATH=%EXTRACT_PATH%\computer-lab-envs-main\tmkt79\init.ps1
 
-echo [101;93m=== TMKT79 environment helper ===[0m
+echo %PROMPT_PREFIX%TMKT79 environment helper%PROMPT_SUFFIX%
 
-echo %PROMPT_PREFIX% Downloading and extracting up-to-date resources...
+echo %PROMPT_PREFIX%Downloading and extracting up-to-date resources...%PROMPT_SUFFIX%
 powershell -ExecutionPolicy Bypass -Command "Invoke-WebRequest %ARCHIVE_URL% -OutFile %ARCHIVE_PATH%" || goto :error
 powershell -ExecutionPolicy Bypass -Command "Expand-Archive -Path %ARCHIVE_PATH% -DestinationPath %EXTRACT_PATH%" -Force || goto :error
 
-echo %PROMPT_PREFIX% Setting up environment...
+echo %PROMPT_PREFIX%Setting up environment...%PROMPT_SUFFIX%
 powershell -ExecutionPolicy Bypass -Command "%INIT_SCRIPT_PATH%"
 
-echo %PROMPT_PREFIX% Done!
+echo %PROMPT_PREFIX%Done!%PROMPT_SUFFIX%
 pause
 goto :EOF
 
