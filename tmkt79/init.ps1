15efc03f7bc7d12871a7d8a1bc9bc2a6fe4cc663
Olle Vidner <olle@vidner.se>
2023-09-01 19:05:06 +0200

    Just say "setup"

--- tmkt79/init.ps1
+++ tmkt79/init.ps1
@@ -1,4 +1,4 @@
-function setupCatiaSettings {
+function setupCatia {
   $catiaEnvPath = Join-Path $PSScriptRoot "catia"
 
   # Brute-force copy the CATSettings that needs to be set with settings
@@ -12,15 +12,15 @@ function setupCatiaSettings {
   Invoke-Item (Join-Path $catiaEnvPath "setup.CATScript")
 }
 
-function setupExcelTrust {
+function setupExcel {
   # Enable access to the VBA object model
   New-ItemProperty -Path "Registry::HKEY_CURRENT_USER\Software\Microsoft\Office\16.0\Excel\Security\" -Name "AccessVBOM" -PropertyType "DWord" -Value 1 -Force
   # Enable all macros
   New-ItemProperty -Path "Registry::HKEY_CURRENT_USER\Software\Microsoft\Office\16.0\Excel\Security\" -Name "VBAWarnings" -PropertyType "DWord" -Value 1 -Force
 }
 
-Write-Host -ForegroundColor Blue "Setting up Excel trust center..."
-setupExcelTrust | Out-Null
+Write-Host -ForegroundColor Blue "* Setting up Excel..."
+setupExcel | Out-Null
 
-Write-Host -ForegroundColor Blue "Setting up CATIA license and settings..."
-setupCatiaSettings | Out-Null
+Write-Host -ForegroundColor Blue "* Setting up CATIA..."
+setupCatia | Out-Null
